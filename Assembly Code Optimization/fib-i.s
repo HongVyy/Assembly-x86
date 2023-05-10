eb43bb205768c91ec7d99e665c8aa9a0a9184498
Vo Hong Vy Le <92348180+HongVyy@users.noreply.github.com>
2023-05-10 13:44:56 -0700

    Create fib-i.s
    Based on the figure on page 2 recursion is not the best way to handle this problem. There are
    many calls that are repeated. As it turns out, the recursive Fibonacci implementation has a run
    time of O (2n)! This problem should be linear O (n) (you can verify for yourself by writing down
    the Fibonacci base cases and calculating the n-th number, you should notice right away you are
    doing a single sweep of the numbers, hence linear runtime).

---
+++ Assembly Code Optimization/fib-i.s
@@ -0,0 +1,61 @@
+.data
+msg1: .asciiz  " Enter number: "
+msg2: .asciiz  " The fibonacci number is: "
+.text
+.globl main
+main: 
+li $v0, 4
+la $a0, msg1
+syscall
+
+li $v0, 5
+syscall 
+
+add $a0, $v0, $zero
+
+jal fib
+move $t0, $v0
+
+li $v0, 4
+la $a0, msg2
+syscall
+
+move $v0, $t0 
+add $a0, $v0, $zero
+
+li $v0, 1
+syscall
+
+li $v0, 10
+syscall
+
+fib: 
+addi $t0, $zero, 1
+
+beqz $a0, return0
+beq $a0, $t0, return1
+
+add $t1, $zero, $zero
+add $t2, $zero, $zero
+
+addi $t3, $zero, 1
+addi $t4, $zero, 1
+loop: 
+
+bge $t4, $a0, endloop
+add $t1, $t2, $t3
+add $t2, $zero, $t3
+add $t3, $zero, $t1
+addi $t4, $t4, 1
+j loop
+
+endloop:
+add $v0, $zero, $t1
+jr $ra
+return0: 
+add $v0, $zero, $zero
+jr $ra
+return1: 
+addi $v0, $zero, 1 
+jr $ra
+
